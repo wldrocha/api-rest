33d10cc755911d2e9a2bcdf99384af26abde7dca
Wladimir Rocha <wld.rocha@gmail.com>
2019-01-08 14:06:46 -0400

    agregado la constante del puerto, bodyParser

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 const express = require('express')
+const bodyParser = require('body-parser')
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000
+
+app.use(bodyParser.urlencoded({extended : false}))
+app.use(bodyParser.json())
+
 app.listen(port, ()=>{
-    console.log('API REST en el puerto') 
+    console.log(`API REST en el puerto ${port}`) 
 })
 
