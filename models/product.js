7827cc42a3561212aa434b46aaffd6c514e527e3
Wladimir Rocha <wld.rocha@gmail.com>
2019-01-10 12:13:39 -0400

    Agregada la carpeta modelos para comenzar a moularizar la app

---
+++ models/product.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+const ProductSchema = Schema({
+    name: String,
+    picture: String,
+    price : {type: Number, default: 0},
+    category: {type: String, enum:['computers', 'phones', 'accesories']},
+    description: String
+})
+
+moongose.model('Product', ProductSchema)
