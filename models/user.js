d1aee74cceb31882bff778065c51568193c97a55
Wladimir Rocha <wld.rocha@gmail.com>
2019-01-11 22:12:05 -0400

    modificación menor

--- models/user.js
+++ models/user.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 const bcrypt = require('bcrypt-nodejs')
 
+
 // Se crea el modelo de la BD
 const UserSchema = new Schema({
     email: {type: String, unique: true, lowercase: true },
