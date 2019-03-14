2ddfc081f8f732352007ab2fab5126ae08a4edab
wld.rocha <wld.rocha@gmail.com>
2019-03-14 19:26:09 -0400

    arreglado error en usuarios

--- controllers/user.js
+++ controllers/user.js
@@ -4,25 +4,28 @@ const service = require('../service')
 function signUp (req, res){
     const user = new User({
         email: req.body.email,
-        displayName: req.body.displayName
+        displayName: req.body.displayname,
+        password: req.body.password
     })
 
     user.save(err=>{
         if(err) res.status(500).sed({ mensaje: `Error al crear el usuario : ${err}` })
 
-        return res.status(200).send({ token: service.createToken(user) })
+        return res.status(201).send({ token: service.createToken(user) })
     })
 }
 
 function signIn(req, res){
-    User.find({email: req.body.email}, (err, user)=>{
-        if(err) return res.status.(500).send({ mensaje: err})
+        User.find({email: req.body.email}, (err, user)=>{
+        if(err) return res.status(500).send({ mensaje: err})
         if(!user) return res.status(404).send({mensaje: 'no existe el usuario'})
-    })
-    req.user = user
-    res.status(200).send({
-        mensaje: 'Te logueaste correctamente',
-        token: service.createToken(user)
+        
+        req.user = user
+       
+        res.status(200).send({
+            mensaje: 'Te logueaste correctamente',
+            token: service.createToken(user)
+        })
     })
 }
 
